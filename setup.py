6ee49533bc2de2ed754bd8bac89212e3b26a5b30
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2018-04-29 16:11:26 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+
+"""
+    setup.py
+    ~~~~~~~~
+
+    Setup script.
+
+    :copyright: (c) 2018 by Ali Ghaffaari.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+import codecs
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Error: 'setuptools' is not installed which is required.",
+          file=sys.stderr)
+    exit(1)
+
+
+_HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Get package release information.
+_PACKAGE_NAME = "vcfy"
+_RELEASE_INFO = {}
+with open(os.path.join(_HERE, _PACKAGE_NAME, 'release.py')) as release_file:
+    exec(release_file.read(), _RELEASE_INFO)  # pylint: disable=exec-used
+
+# Get the long description from the README file.
+with codecs.open(os.path.join(_HERE, 'README.rst'), encoding='utf-8') as rm_f:
+    LONG_DESCRIPTION = rm_f.read()
+
+_PYPINAME = "vcfy"
+_PACKAGES = find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests'])
+_GITHUB_BASE = "https://github.com/cartoonist/"
+_VCS_URL = _GITHUB_BASE + _PYPINAME
+_TAR_URL = _VCS_URL + "/tarball/" + _RELEASE_INFO['__version__']
+
+setup(
+    name=_PYPINAME,
+    packages=_PACKAGES,
+    version=_RELEASE_INFO['__version__'],
+    description=_RELEASE_INFO['__description__'],
+    long_description=LONG_DESCRIPTION,
+    author=_RELEASE_INFO['__author__'],
+    author_email=_RELEASE_INFO['__email__'],
+    license=_RELEASE_INFO['__license__'],
+    url=_VCS_URL,
+    download_url=_TAR_URL,
+    keywords=_RELEASE_INFO['__keywords__'],
+    classifiers=_RELEASE_INFO['__classifiers__'],
+    install_requires=_RELEASE_INFO['__requires__'],
+    tests_require=_RELEASE_INFO['__tests_require__'],
+    extras_require=_RELEASE_INFO['__extras_require__'],
+    entry_points=_RELEASE_INFO['__entry_points__'],
+)
