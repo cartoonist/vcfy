04c38a99e6af04df5fc3de07669dd84aab911a6f
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2018-04-29 16:10:39 +0200

    Add cli module
    which implements the command-line interface.

---
+++ vcfy/cli.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+"""
+    vcfy.cli
+    ~~~~~~~~
+
+    This module implements command-line interface.
+
+    :copyright: (c) 2018 by Ali Ghaffaari.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import click
+
+
+@click.command()
+def cli(**kwargs):
+    """Command line interface."""
+    pass
