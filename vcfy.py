608180ad41a823ccaee122e2d258732a977bed71
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2018-04-29 10:46:48 +0200

    Initial commit

---
+++ vcfy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+    vcfy
+    ~~~~
+
+    Generate a VCF file with random variants using the given probability model.
+
+    :copyright: (c) 2018 by Ali Ghaffaari.
+    :license: MIT, see LICENSE for more details.
+"""
