af1b7239e67f006ec94f1c34c8819e2298df8051
Ali Ghaffaari <ali.ghaffaari@gmail.com>
2018-04-29 16:05:52 +0200

    Go deeper
    Add one-level of hierarchy to the project source directory.

---
+++ vcfy/model.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+    vcfy.model
+    ~~~~~~~~~~
+
+    This module implements the model for generating variants based on the given
+    parameters.
+
+    :copyright: (c) 2018 by Ali Ghaffaari.
+    :license: MIT, see LICENSE for more details.
+"""
